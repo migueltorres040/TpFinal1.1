6335333302378770bf2d85ed2312a8cef9b0e512
migueltorres040 <migueltorres040@gmail.com>
2017-05-27 04:11:37 -0300

    este componente permite hacer una validacion asincronica, este tipo de validador se utiliza para verificar informacion en una BD antes de guardar. -valido si el id ya se encuentra en la BD. -creo en usuario.servicie una funcion de consulta para id

--- alquiler/src/app/usuarios/usuarios.validators.ts
+++ alquiler/src/app/usuarios/usuarios.validators.ts
@@ -1,3 +1,4 @@
+// este es es valididacion asincronica. permite verificar si se encuntra en la base 
 import{AbstractControl,ValidatorFn,Validators} from '@angular/forms';
 import{UsuariosService} from './usuarios.service';
 
